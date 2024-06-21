8276eb8fc3681f3bb3b0d4594cf7b5f387cb034e
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-21 18:34:03 +0330

    added astrocommunity plugins

---
+++ lua/community.lua
@@ -0,0 +1,10 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+}
