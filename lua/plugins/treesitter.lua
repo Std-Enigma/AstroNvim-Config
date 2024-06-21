0b308da5d9d6f49cc74b4d19eee0eab9f3b6a662
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-21 18:33:52 +0330

    modified treesitter parsers

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "cpp",
+      "lua",
+      "rust",
+      "java",
+      "python",
+      "c_sharp",
+    })
+  end,
+}
