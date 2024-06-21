a817528988c5da84274387a5ade11106bcda9189
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-21 18:33:12 +0330

    added astrocore plugin spec

---
+++ lua/plugins/astrocore.lua
@@ -0,0 +1,28 @@
+return {
+  "AstroNvim/astrocore",
+  opts = {
+    mappings = {
+      n = {
+        -- navigate buffer tabs with `H` and `L`
+        L = {
+          function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+          desc = "Next buffer",
+        },
+        H = {
+          function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+          desc = "Previous buffer",
+        },
+
+        -- Buffer picker for closing them
+        ["<Leader>bD"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Pick to close",
+        },
+      },
+    },
+  },
+}
