406f911c440649cf08c4789c07bbe5bd75866f0a
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-07-25 13:17:51 +0330

    add inc-rename-nvim to community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -2,6 +2,7 @@ return {
 	"AstroNvim/astrocommunity",
 	{ import = "astrocommunity.utility.noice-nvim" },
 	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.lsp.inc-rename-nvim" },
 	{ import = "astrocommunity.completion.cmp-cmdline" },
 	{ import = "astrocommunity.scrolling.vim-smoothie" },
 	{ import = "astrocommunity.colorscheme.moonfly" },
