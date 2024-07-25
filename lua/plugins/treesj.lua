0577b6f0d89e5d8b9e5a4933b8df53bee9bbb1d5
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-07-25 16:10:02 +0330

    added treesj plugin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,6 @@
+return {
+	"Wansmer/treesj",
+	cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+	opts = { use_default_keymaps = false },
+	keys = { { "gm", "<Cmd>TSJToggle<CR>", desc = "Split/Join code block" } },
+}
